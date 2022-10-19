7da2641d525a03152e94af427c789405dc69a3ad
Cristina_Zarinsh <kot-199613@mail.ru>
2022-10-19 23:29:01 +0300

    Добавлено решение задачи

--- Program.cs
+++ Program.cs
@@ -2,10 +2,11 @@
 //создать массив из строк, в который попадут только символы, не являющиеця цифрами.
 
 Console.Clear();
-char[] data = ("Ка6кой-то88 ран5домны!й те90кст").ToCharArray();
+char[] data = ("Ка6кой-то88 ран5домны55й те90кст").ToCharArray();
 var result = string.Empty;
 foreach(var item in data)
 {
 if(!char.IsDigit(item))
 result+= item;
 }
+Console.WriteLine(result);
